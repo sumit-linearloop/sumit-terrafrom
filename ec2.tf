d9819fedc111334fcebf5feefca617ce2bb96af2
sumit-linearloop <sumit@linearloop.io>
2024-11-18 15:45:30 +0530

    a

--- ec2.tf
+++ ec2.tf
@@ -1,64 +1,3 @@
-terraform {
-  backend "remote" {
-    organization = "CI_CD"
-    workspaces {
-      name = "Terrafrom-CI_CD"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
-# Create an EC2 instance
-resource "aws_instance" "worker" {
-  ami           = var.ami            # Reference the AMI variable
-  instance_type = var.instance_type  # Reference the instance type variable
-  key_name      = var.key_name       # Reference the SSH key pair variable
- 
-  # Add security group
-  security_groups = [aws_security_group.sumit-iac.name]
- 
-  tags = {
-    Name = "sumit-cloud"
-  }
-}
- 
-# Create a Security Group (SG) for the EC2 instance
-resource "aws_security_group" "sumit-iac" {
-  name        = "sumit-iac"
-  description = "sumit-iac"
- 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere
-  }
- 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
-  }
- 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic
-  }
-}
- 
-output "instance_public_ip" {
-  value = aws_instance.worker.public_ip
-}
-
-
-
-
 # terraform {
 #   backend "remote" {
 #     organization = "CI_CD"
@@ -72,74 +11,135 @@ output "instance_public_ip" {
 #   region = "ap-south-1"
 # }
 
-# # Security Group
+# # Create an EC2 instance
+# resource "aws_instance" "worker" {
+#   ami           = var.ami            # Reference the AMI variable
+#   instance_type = var.instance_type  # Reference the instance type variable
+#   key_name      = var.key_name       # Reference the SSH key pair variable
+ 
+#   # Add security group
+#   security_groups = [aws_security_group.sumit-iac.name]
+ 
+#   tags = {
+#     Name = "sumit-cloud"
+#   }
+# }
+ 
+# # Create a Security Group (SG) for the EC2 instance
 # resource "aws_security_group" "sumit-iac" {
 #   name        = "sumit-iac"
-#   description = "Security Group for EC2 instance"
-
+#   description = "sumit-iac"
+ 
 #   ingress {
 #     from_port   = 22
 #     to_port     = 22
 #     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
+#     cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere
 #   }
-
+ 
 #   ingress {
 #     from_port   = 80
 #     to_port     = 80
 #     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
+#     cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
 #   }
-
+ 
 #   egress {
 #     from_port   = 0
 #     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic
 #   }
 # }
-
-# # EC2 Instance
-# resource "aws_instance" "worker" {
-#   ami           = var.ami
-#   instance_type = var.instance_type
-#   key_name      = var.key_name
-#   security_groups = [aws_security_group.sumit-iac.name]
-
-#   tags = {
-#     Name = "sumit-cloud"
-#   }
-
-#   # Upload the shell script to the instance
-#   provisioner "file" {
-#     source      = "./sumit.sh" # Path to your local script
-#     destination = "/home/ubuntu/sumit.sh" # Destination path on the instance
-#   }
-
-#   connection {
-#     type        = "ssh"
-#     host        = self.public_ip
-#     user        = "ubuntu" # Update to ubuntu for Ubuntu AMI
-#     private_key = var.ssh_private_key # Use variable directly
-#   }
-
-#   # Execute the uploaded script
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x /home/ubuntu/sumit.sh",
-#       "sudo /home/ubuntu/sumit.sh"
-#     ]
-
-#     connection {
-#       type        = "ssh"
-#       host        = self.public_ip
-#       user        = "ubuntu" # Use ubuntu user
-#       private_key = var.ssh_private_key # Use variable directly
-#     }
-#   }
-# }
-
-# # Output the instance public IP
+ 
 # output "instance_public_ip" {
 #   value = aws_instance.worker.public_ip
 # }
+
+
+
+
+terraform {
+  backend "remote" {
+    organization = "CI_CD"
+    workspaces {
+      name = "Terrafrom-CI_CD"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Security Group
+resource "aws_security_group" "sumit-iac" {
+  name        = "sumit-iac"
+  description = "Security Group for EC2 instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "worker" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [aws_security_group.sumit-iac.name]
+
+  tags = {
+    Name = "sumit-cloud"
+  }
+
+  # Upload the shell script to the instance
+  provisioner "file" {
+    source      = "./sumit.sh" # Path to your local script
+    destination = "/home/ubuntu/sumit.sh" # Destination path on the instance
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu" # Update to ubuntu for Ubuntu AMI
+    private_key = var.ssh_private_key # Use variable directly
+  }
+
+  # Execute the uploaded script
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ubuntu/sumit.sh",
+      "sudo /home/ubuntu/sumit.sh"
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu" # Use ubuntu user
+      private_key = var.ssh_private_key # Use variable directly
+    }
+  }
+}
+
+# Output the instance public IP
+output "instance_public_ip" {
+  value = aws_instance.worker.public_ip
+}
