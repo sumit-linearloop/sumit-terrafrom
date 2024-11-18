2707380d84419129522c43cd3e5388227003db45
sumit-linearloop <sumit@linearloop.io>
2024-11-18 12:11:48 +0530

    var

--- variables.tf
+++ variables.tf
@@ -17,3 +17,15 @@ variable "ssh_private_key" {
   description = "Path to the private key file for SSH connection"
   type        = string
 }
+
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
