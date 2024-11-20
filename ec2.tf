be5d2063eab681288f5b3e520faf5ada6cf50246
sumit-linearloop <sumit@linearloop.io>
2024-11-20 10:34:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -11,52 +11,68 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# Create an EC2 instance
+
+
+# Security Group
+resource "aws_security_group" "sumit-iac" {
+  name        = "sumit-iac"
+  description = "sumit-iac security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere (change to restrict access)
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere (change to restrict access)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
 resource "aws_instance" "worker" {
-  ami           = var.ami            # Reference the AMI variable
-  instance_type = var.instance_type  # Reference the instance type variable
-  key_name      = var.key_name       # Reference the SSH key pair variable
-
-  associate_public_ip_address = true  # Ensure public IP is assigned
-
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  associate_public_ip_address = true
   security_groups = [aws_security_group.sumit-iac.name]
 
   tags = {
     Name = "sumit-cloud"
   }
-}
 
-# Create a Security Group (SG) for the EC2 instance
-resource "aws_security_group" "sumit-iac" {
-  name        = "sumit-iac"
-  description = "sumit-iac"
- 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere
-  }
- 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
-  }
- 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"           # Use "-1" to allow all protocols
-    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic
+  # Remote Exec Provisioner
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y awscli",
+      "aws --version"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = var.username
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
   }
 }
 
+# Output
 output "instance_public_ip" {
   value = aws_instance.worker.public_ip
 }
-
 
 
 
