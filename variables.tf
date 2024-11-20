6062d4b363a1f8b151150405abb1fab30352ac15
sumit-linearloop <sumit@linearloop.io>
2024-11-20 15:56:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,14 +24,3 @@ variable "username" {
   default     = "ubuntu"
 }
 
-variable "aws_access_key_id" {
-  description = "AWS Access Key ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  sensitive   = true
-}
