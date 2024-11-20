90b1dc71b4785e5f100ff86706d1d8482b2fba92
sumit-linearloop <sumit@linearloop.io>
2024-11-20 11:28:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,9 +9,6 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1"
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-
 }
 
 # Security Group
@@ -63,12 +60,6 @@ resource "aws_instance" "worker" {
       "unzip awscliv2.zip",                       # Unzip the AWS CLI installer
       "sudo ./aws/install",                       # Install AWS CLI
       "aws --version",                            # Verify AWS CLI installation
-      "echo 'Configuring AWS CLI...'",            # Configure AWS CLI
-      "aws configure set aws_access_key_id ${var.aws_access_key_id}",
-      "aws configure set aws_secret_access_key ${var.aws_secret_access_key}",
-      "aws configure set region ap-south-1",
-      "echo 'AWS CLI configured.'",
-      "aws s3 ls",                               # List S3 buckets
     ]
 
     connection {
@@ -78,11 +69,6 @@ resource "aws_instance" "worker" {
       host        = self.public_ip            # EC2 public IP for SSH connection
     }
   }
-}
-
-# Output
-output "instance_public_ip" {
-  value = aws_instance.worker.public_ip
 }
 
 
